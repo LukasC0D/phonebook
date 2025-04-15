8f7758f66bece2830068fb1e8c2ed9e08d8bb698
LukasC0D <kaupasa666@gmail.com>
2025-04-15 10:39:44 +0300

    login, register blade forms

---
+++ backend/resources/views/auth/login.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container mt-5 d-flex justify-content-center">
+    <div class="bg-dark p-4 rounded" style="width: 300px;">
+        <h2 class="text-light text-center mb-4">Login</h2>
+        <form method="POST" action="/login" class="d-flex flex-column align-items-center">
+            @csrf
+            <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required class="form-control mb-3 text-center">
+            <input type="password" name="password" placeholder="Password" required class="form-control mb-3 text-center">
+            <button class="btn btn-primary m-1" type="submit">Login</button>
+        </form>
+    </div>
+</div>
+@endsection
---
+++ backend/resources/views/auth/register.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container d-flex justify-content-center mt-5">
+        <div class="bg-dark p-4 rounded" style="width: 300px;">
+            <div class="text-center text-light">
+                <h2>Register</h2>
+            </div>
+            <form method="POST" action="/register" class="d-flex flex-column align-items-center">
+                @csrf
+                <br><input type="text" name="name" placeholder="Name" value="{{ old('name') }}" required class="form-control text-center">
+                <br><input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required class="form-control text-center">
+                <br><input type="password" name="password" placeholder="Password" required class="form-control text-center">
+                <br><input type="password" name="password_confirmation" placeholder="Confirm Password" required class="form-control text-center">
+                <br><button class="btn btn-primary m-1" type="submit">Register</button>
+            </form>
+        </div>
+    </div>
+@endsection
